9e1e928b7972abf9f24a83771ce7e42c715d25e5
shemil01 <shemilmk1033@gmail.com>
2024-04-19 16:01:07 +0530

    updated

---
+++ 1. Two Sum.js
@@ -0,0 +1,13 @@
+var twoSum = function(nums, target) {
+    let mp = new Map()
+ 
+ for (let i = 0; i < nums.length; i++) {
+     let diff = target - nums[i]
+     
+     if (mp.has(diff)) {
+         return [i, mp.get(diff)]
+     }
+     
+     mp.set(nums[i], i)
+ }
+};
